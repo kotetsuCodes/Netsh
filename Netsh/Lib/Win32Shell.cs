a9a4242ff24c43db30f9fde422974fa3bd12ed54
Carl Brown <kotetsuCodes@gmail.com>
2018-02-19 16:39:12 -0600

    Update Win32Shell.cs
    Updated comment to better reflect source of private method

--- Netsh/Lib/Win32Shell.cs
+++ Netsh/Lib/Win32Shell.cs
@@ -48,7 +48,7 @@ namespace Netsh.Lib
       }
     }
 
-    //https://stackoverflow.com/questions/14094771/problems-getting-desired-output-from-process-start
+    // adapted small portion from https://stackoverflow.com/questions/14094771/problems-getting-desired-output-from-process-start
     private void readOutput(object sender, DataReceivedEventArgs e)
     {
       if(e.Data != null)
