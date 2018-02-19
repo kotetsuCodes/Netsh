ece2b3aeca2d419cd04b0dd8d46ee69ef6533ec3
Carl Brown <carlb@hostek.com>
2018-02-19 16:17:57 -0600

    Changed process to utilize using statement to autodispose. Added events for reading stdout and stderr. Changed Win32Shell to non-static class to support storing standard output and standard error.

--- Netsh/Lib/Win32Shell.cs
+++ Netsh/Lib/Win32Shell.cs
@@ -5,8 +5,17 @@ using System.Text;
 
 namespace Netsh.Lib
 {
-  public static class Win32Shell
+  public class Win32Shell
   {
+    public Win32Shell()
+    {
+      this.StandardOutput = new List<string>();
+      this.StandardError = new List<string>();
+    }
+
+    public List<string> StandardOutput { get; private set; }
+    public List<string> StandardError { get; private set; }
+
     /// <summary>
     /// Method for executing processes on Windows
     /// </summary>
@@ -14,29 +23,46 @@ namespace Netsh.Lib
     /// <param name="arguments"></param>
     /// <param name="standardOut"></param>
     /// <param name="standardError"></param>
-    public static void Exec(string executable, string arguments, out List<string> standardOut, out List<string> standardError)
+    public List<string> Exec(string executable, string arguments)
     {
-      standardOut = new List<string>();
-      standardError = new List<string>();
-
-      var processStartInfo = new ProcessStartInfo()
+      using(var process = new Process())
       {
-        UseShellExecute = false,
-        RedirectStandardError = true,
-        RedirectStandardOutput = true,
-        Arguments = arguments,
-        WindowStyle = ProcessWindowStyle.Hidden,
-        FileName = executable
-      };
+        var start = new ProcessStartInfo(executable, arguments);
 
-      var process = Process.Start(processStartInfo);
+        start.WindowStyle = ProcessWindowStyle.Hidden;
+        start.CreateNoWindow = true;
+        start.UseShellExecute = false;
+        start.RedirectStandardOutput = true;
+        start.RedirectStandardError = true;
 
-      while(process.StandardOutput.Peek() > -1)
-      {
-        standardOut.Add(process.StandardOutput.ReadLine());
+        process.OutputDataReceived += new DataReceivedEventHandler(readOutput);
+        process.ErrorDataReceived += new DataReceivedEventHandler(readError);
+
+        process.StartInfo = start;
+        process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+        process.WaitForExit();
+
+        return this.StandardOutput;
       }
+    }
 
-      process.WaitForExit();
+    //https://stackoverflow.com/questions/14094771/problems-getting-desired-output-from-process-start
+    private void readOutput(object sender, DataReceivedEventArgs e)
+    {
+      if(e.Data != null)
+      {
+        this.StandardOutput.Add(e.Data);
+      }
+    }
+
+    private void readError(object sender, DataReceivedEventArgs e)
+    {
+      if(e.Data != null)
+      {
+        this.StandardError.Add(e.Data);
+      }
     }
   }
 }
