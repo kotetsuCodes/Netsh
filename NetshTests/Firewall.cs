e0daddc8e6f88acecd27e947efe93b6d2b0a0eb4
Carl Brown <carlb@hostek.com>
2018-02-20 12:40:08 -0600

    Added firewall creation test methods for success and fail

--- NetshTests/Firewall.cs
+++ NetshTests/Firewall.cs
@@ -7,12 +7,44 @@ namespace NetshTests
   [TestClass]
   public class Firewall
   {
+    private string getGeneratedTestRuleName()
+    {
+      return $"{Guid.NewGuid().ToString()}_NetshTesting";
+    }
+
     [TestMethod]
     public void GetFirewallRules()
     {
       var rules = Netsh.Firewall.Rules.GetFirewallRules();
 
       Assert.IsTrue(rules.Any());
+    }
+
+    [TestMethod]
+    public void CreateFirewallRuleShouldSucceed()
+    {
+      Netsh.Firewall.Rules.CreateFirewallRule(new Netsh.Firewall.Rule
+      {
+        Name = $"{Guid.NewGuid().ToString()}_NetshTesting",
+        Direction = "in",
+        Action = "allow",
+        Protocol = "TCP",
+        LocalPort = "7777"
+      });
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void CreateFirewallRuleShouldFail()
+    {
+      Netsh.Firewall.Rules.CreateFirewallRule(new Netsh.Firewall.Rule
+      {
+        Name = getGeneratedTestRuleName(),
+        Direction = "this should fail",
+        Action = "this should fail",
+        Protocol = "this should fail",
+        LocalPort = "this should fail"
+      });
     }
   }
 }
